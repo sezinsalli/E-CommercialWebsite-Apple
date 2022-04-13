1d5390ad169d211ba0ebdd13fb4e05d30aa28bfe
sezin inyayla <95285144+sezins@users.noreply.github.com>
2022-04-13 21:53:14 +0300

    +++

---
+++ apple/script.js
@@ -0,0 +1,17 @@
+const slideshowDivs = () => {
+
+    for (let i = 1; i <= 5; i++) {
+
+        const div = document.createElement('div')
+
+        div.style.backgroundImage = `url(slideshow\section-1-bg-${1}.jpg`
+        İ == 1 && div.classList.add('change')
+
+        document.querySelector('.slideshow')
+
+        appendChild(div)
+
+    }
+
+}
+slideshowDivs()
